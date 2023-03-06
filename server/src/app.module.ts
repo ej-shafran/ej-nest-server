a94fe756856868a0910f89f2e733c49be16e5de0
ej-shafran <ejshafran@hilma.tech>
2023-03-06 15:07:43 +0200

    Full structure

---
+++ server/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+TYPEORM IMPORT PLACEHOLDER
+
+@Module({
+  imports: [
+    TYPEORM MODULE PLACEHOLDER
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
